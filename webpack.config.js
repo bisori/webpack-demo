2e657e35216c191f14c28da84bdd4e6ad1a1e3d9
bisori <bisori@coupang.com>
2019-12-27 16:38:58 +0900

    study: Asset Management https://webpack.js.org/guides/asset-management/

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,29 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
 };
